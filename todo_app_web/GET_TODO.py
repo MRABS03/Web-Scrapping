5729393eaf47f05be2e8104ef5231848bf7c0a1c
Abdullah Bin Salman <90260374+MRABS03@users.noreply.github.com>
2024-09-09 19:20:21 +0500

    Delete todo_app_web directory

--- todo_app_web/GET_TODO.py
+++
@@ -1,7 +0,0 @@
-filepath= fr'D:\User\Documents\ML\Python Mega Course\todo_app_web\todo.txt'
-
-
-def get_todos():
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-        return todos
--- todo_app_web/WRITE_TODO.py
+++
@@ -1,7 +0,0 @@
-
-filepath = fr'D:\User\Documents\ML\Python Mega Course\todo_app_web\todo.txt'
-
-
-def write_todos(task):
-    with open(filepath, "w") as file:
-        file.writelines(task)
